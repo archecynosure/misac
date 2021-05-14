7f1a70823a81ff730f0eabe194e288022608af2d
archecynosure <archecynosure@gmail.com>
2021-05-14 23:55:23 +0200

    working

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ function Header() {
         <div className="header">
         <h1>This is the header component</h1>
         <h2>What is going on</h2>
+        <h3>It seems to be working now</h3>
             
         </div>
     )
