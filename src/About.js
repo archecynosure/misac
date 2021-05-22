5fe3bafc84386ceef28b16b6bcfc361b76cbe9c9
archecynosure <archecynosure@gmail.com>
2021-05-22 23:08:15 +0200

    added router functionality

--- src/About.js
+++ src/About.js
@@ -4,21 +4,21 @@ import './About.css'
 function About() {
     return (
         <div className="about">
-        <div className="about__item">
-            <img src="images\customer-service-agent.svg" alt="call center icon"/>
-            <p>DOSTUPNOST</p>
-            <p>24/7 call centar</p>
-        </div>
-        <div className="about__item">
-            <img src="images\shield.svg" alt="security icon"/>
-            <p>SIGURNOST</p>
-            <p>bezbedne transakcije</p>
-        </div>
-        <div className="about__item">
-            <img src="images\delivery-truck.svg" alt="delivery icon"/>
-            <p>BRZINA</p>
-            <p>isporuka u najkraćem roku</p>
-        </div>
+            <div className="about__item">
+                <img src="images\customer-service-agent.svg" alt="call center icon" />
+                <p>DOSTUPNOST</p>
+                <p>24/7 call centar</p>
+            </div>
+            <div className="about__item">
+                <img src="images\shield.svg" alt="security icon" />
+                <p>SIGURNOST</p>
+                <p>bezbedne transakcije</p>
+            </div>
+            <div className="about__item">
+                <img src="images\delivery-truck.svg" alt="delivery icon" />
+                <p>BRZINA</p>
+                <p>isporuka u najkraćem roku</p>
+            </div>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header.js"
 import Landing from "./Landing.js"
 import Featured from "./Featured.js"
@@ -9,14 +10,25 @@ import Description from './Description.js'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Landing />
-      <Description />
-      <Featured />
-      <About />
-      <ContactUs />
-    </div>
+    <Router>
+      <div className="App">
+        < Switch >
+          <Route path="/login" >
+            <Landing />
+          </Route>
+
+          <Route path="/" >
+            <Header />
+            <Landing />
+            <Description />
+            <Featured />
+            <About />
+            <ContactUs />
+          </Route>
+        </Switch >
+
+      </div>
+    </Router>
   );
 }
 
